d7b4ce9ab2c2d447bfd8119be871014a1c3a85d7
rohitwtbs <12rohit4@gmail.com>
2020-08-02 22:12:09 +0530

    added keyfram animation to paragraph text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>Work in progress...</h2>
+    <p>Work in progress...</p>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <EllipseComponent/>
@@ -26,5 +26,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+p {
+  animation-duration: 3s;
+  animation-name: slidein;
+  font-size: 50px;
+  font-style: bold;
+}
+
+@keyframes slidein {
+  from {
+    margin-left: 100%;
+    width: 300%; 
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
 }
 </style>
